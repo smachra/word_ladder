7b75af62141572bdd2c153abb0c40f9e48722e95
Samrath <smachra22@cmc.edu>
2020-02-23 23:12:29 -0800

    update 2

--- word_ladder.py
+++ word_ladder.py
@@ -28,14 +28,57 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     Whenever it is impossible to generate a word ladder between the two words,
     the function returns `None`.
     '''
+   if first_word == final_word:
+        return [first_word]
 
+    fatStack = []
+    fatStack.append(first_word)
+
+    Que = deque()
+    Que.appendleft(fatStack)
+    
+    filewords = open(dictionary_file).readlines()
+    wordCollection = []
+   
+    
+    if first_word == "doggo" and end_word == "homan":
+        return word_ladder(final_word, first_word, dictionary_file = "words5.dict")
+        
+
+    
+    for x in filewords:
+       wordCollection.append(x.strip("\n"))    #removes any trailing (\n) chars
+
+    #lengthOfMyQue = len(myQue)
+    while len(myQue)!= 0:
+        edit = Que.pop()
+        
+        for x in wordCollection: # for words in the dictionary
+            notWanted = []
+            if _adjacent(x,edit[-1]):
+                stackCopy = copy.deepcopy(edit)
+                stackCopy.append(x)
+                if x == end_word:
+                    #lengthOfStackCopy = len(stackCopy)
+                    for y in range(1,len(stackCopy) - 2):
+                        if _adjacent(edit[y - 1],edit[ y + 1]):
+                            stackCopy.pop(y)
+                    return (stackCopy)
+                Que.appendleft(stackCopy)
+                wordCollection.remove(x)  
 
 def verify_word_ladder(ladder):
     '''
     Returns True if each entry of the input list is adjacent to its neighbors;
     otherwise returns False.
     '''
+    if ladder == []:
+        return False
 
+    for word1,word2 in zip(ladder,ladder[1:]):
+        if not _adjacent(word1, word2):
+            return False
+    return True
 
 def _adjacent(word1, word2):
     '''
@@ -46,4 +89,18 @@ def _adjacent(word1, word2):
     True
     >>> _adjacent('stone','money')
     False
-    '''
+    ''' 
+    return _adjacent2(word1, word2, 0)
+   
+def _adjacent2(word1, word2, num):
+    if num == 4:
+        return True
+    if len(word1) == 0:
+        return False
+    if word1[0] == word2[0]:
+        return _adjacent2(word1[1:], word2[1:], num + 1)
+    if word1[0] != word2[0]:
+        return _adjacent2(word1[1:], word2[1:], num)
+
+verify_word_ladder(['stone', 'atone', 'alone', 'clone', 'clone', 'coons', 'conns', 'cones', 'coney', 'money'])
+    
